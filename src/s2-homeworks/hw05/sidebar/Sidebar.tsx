3e8f288de640659e291ba05fa530d3e39c82054e
Tommy <dfgthii@gmail.com>
2022-11-01 17:19:39 +0300

    hw5 end

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -13,6 +13,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
 
+    const activeClassName = ({isActive}: { isActive: boolean }) => (isActive ? s.act : s.def);
+
     return (
         <>
             {/*затемнение справа от открытого меню*/}
@@ -32,7 +34,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        className={ (isActive)=> isActive.isActive ? s.act : s.def}
+                        className={activeClassName}
+                        end
                         // className={...} // делает студент
                     >
                         Pre-junior
@@ -41,7 +44,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        className={ (isActive)=> isActive.isActive ? s.act : s.def}
+                        className={activeClassName}
+                        end
                         // className={...} // делает студент
                     >
                         Junior
@@ -50,7 +54,8 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        className={ (isAct) => isAct.isActive ? s.act : s.def}
+                        className={activeClassName}
+                        end
                         // className={...} // делает студент
                     >
                         Junior Plus
